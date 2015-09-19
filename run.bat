b7a8e3673cfce5a1353b98e1c3a039dea85ae769
xheomar <yoga.yogich@gmail.com>
2015-09-19 20:19:18 +0300

    Changed something

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-
+:loop
 
 phantomjs.exe getPoints.js http://www.sports.ru/fantasy/football/team/1553274.html pr-positive > file.txt
 
@@ -13,3 +13,11 @@ phantomjs.exe getPoints.js http://www.sports.ru/fantasy/football/team/1562633.ht
 phantomjs.exe getPoints.js http://www.sports.ru/fantasy/football/team/1557039.html PrincipessaMilana >> file.txt
 
 sort -r file.txt > result.txt
+
+git add result.txt
+
+git commit -m "Again and again..."
+
+git push -u origin master
+
+goto :loop
