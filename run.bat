4fcbe9a43f857c71e382122a6ca28bc85d3dd8fb
xheomar <yoga.yogich@gmail.com>
2015-09-20 20:42:22 +0300

    Deleted empty strings

--- run.bat
+++ run.bat
@@ -1,23 +1,11 @@
 :loop
-
 phantomjs.exe getPoints.js http://www.sports.ru/fantasy/football/team/1553274.html pr-positive > result.txt
-
 phantomjs.exe getPoints.js http://www.sports.ru/fantasy/football/team/1557043.html ymat >> result.txt
-
 phantomjs.exe getPoints.js http://www.sports.ru/fantasy/football/team/1559624.html xheo >> result.txt
-
 phantomjs.exe getPoints.js http://www.sports.ru/fantasy/football/team/1558719.html busotir >> result.txt
-
 phantomjs.exe getPoints.js http://www.sports.ru/fantasy/football/team/1562633.html cron314 >> result.txt
-
 phantomjs.exe getPoints.js http://www.sports.ru/fantasy/football/team/1557039.html PrincipessaMilana >> result.txt
-
-:: sort -r file.txt > result.txt
-
 git add result.txt
-
 git commit -m "Again and again..."
-
 git push -u origin master
-
 goto :loop
